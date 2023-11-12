376d938d57b9ca56be96c7ba7c0eb24d46491e86
BrotherFromAnotherMother <aleksandr.nidodin@gmail.com>
2023-11-12 17:07:48 +0300

    Сделал поля необязательными

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,9 +2,9 @@ from django.db import models  # noqa F401
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200, verbose_name='Название на русском')
-    title_en = models.CharField(max_length=200, null=True, verbose_name='Название на английском')
-    title_jp = models.CharField(max_length=200, null=True, verbose_name='Название на японском')
+    title = models.CharField(blank=True, max_length=200, verbose_name='Название на русском')
+    title_en = models.CharField(blank=True, max_length=200, null=True, verbose_name='Название на английском')
+    title_jp = models.CharField(blank=True, max_length=200, null=True, verbose_name='Название на японском')
     description = models.TextField(blank=True, verbose_name='Описание')
     image = models.ImageField(null=True, blank=True, verbose_name='Картинка')
     previous_evolution = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True,
@@ -20,8 +20,8 @@ class PokemonEntity(models.Model):
     lon = models.FloatField(verbose_name='Долгота')
     appeared_at = models.DateTimeField(default=None, verbose_name='Появится/появится')
     disappeared_at = models.DateTimeField(default=None, verbose_name='Исчезнет/исчез')
-    level = models.IntegerField(null=True, verbose_name='Уровень')
-    health = models.IntegerField(null=True, verbose_name='Здоровье')
-    strength = models.IntegerField(null=True, verbose_name='Сила')
-    defence = models.IntegerField(null=True, verbose_name='Защита')
-    stamina = models.IntegerField(null=True, verbose_name='Выносливость')
+    level = models.IntegerField(blank=True, null=True, verbose_name='Уровень')
+    health = models.IntegerField(blank=True, null=True, verbose_name='Здоровье')
+    strength = models.IntegerField(blank=True, null=True, verbose_name='Сила')
+    defence = models.IntegerField(blank=True, null=True, verbose_name='Защита')
+    stamina = models.IntegerField(blank=True, null=True, verbose_name='Выносливость')
